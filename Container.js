44178d66b631cdedcace826e531bdfe5cb27550e
Brijesh Yadav <88343913+brijeshyadav2312@users.noreply.github.com>
2022-11-03 17:50:18 +0530

    Update Container.js

--- Container.js
+++ Container.js
@@ -3,8 +3,8 @@ const Container = (props) => {
     <div className="card">
         <div style={{backgroundColor:`${props.color_name}`}}></div>
         <span>
-            <h2>{color_name}</h2>
-            <h4 style={{color:`${props.color_name}`}}>{color_id}</h4>
+            <h2>{props.color_name}</h2>
+            <h4 style={{color:`${props.color_name}`}}>{props.color_id}</h4>
         </span>
     </div>
   )
